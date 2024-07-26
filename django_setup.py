232e166af5079edb8e59635ace7775dfe8f8dae1
vrashaVer <da.programs.vr@gmail.com>
2024-07-27 00:51:33 +0300

    create first app
    done 1 task
    create django app task1
    create task1_main file

---
+++ django_setup.py
@@ -0,0 +1,5 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","django_project1.settings")
+django.setup()
---
+++ task1_main.py
@@ -0,0 +1,42 @@
+import django_setup
+from task1.models import Role,User
+
+
+# Створення і збереження ролей
+
+# admin_role = Role.objects.create(name=Role.ADMIN)
+# user_role = Role.objects.create(name=Role.USER)
+# admin_role.save()
+# user_role.save()
+
+
+
+# Створення нових користувачів та присвоєння ролі
+
+role_user = Role.objects.get(id=2)
+role_admin = Role.objects.get(id=1)
+
+# user1 = User(name="User1",
+#              email = "example1@gmail.com",
+#              role = role_admin)
+# user2 =User(name="User2",
+#              email = "example2@gmail.com",
+#              role = role_user)
+# user3 = User(name="User3",
+#              email = "example3@gmail.com",
+#              role = role_user)
+
+# user1.save()
+# user2.save()
+# user3.save()
+
+
+# Зміна ролі
+
+
+# user_test1 = User.objects.get(id=2)
+# user_test1.role = role_admin
+# user_test1.save()
+
+
+
